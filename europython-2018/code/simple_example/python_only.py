90a26da201e0cd7b642a3f8770ab86d2e8c2d4fe
Sven-Hendrik Haase <svenstaro@gmail.com>
2018-07-24 20:53:00 +0200

    More progress yet

---
+++ europython-2018/code/simple_example/python_only.py
@@ -0,0 +1,16 @@
+from math import sqrt
+from random import random
+
+def in_circle(x, y):
+    return sqrt(x*x + y*y) <= 1.0
+
+def calc(iterations):
+    hits = 0
+    for i in range(1, iterations):
+        if in_circle(random(), random()):
+            hits += 1
+
+    pi = 4 * (hits / float(iterations))
+    return pi
+
+print(calc(10**9))
---
+++ europython-2018/code/simple_example/with_rust.py
@@ -0,0 +1,2 @@
+from simple_example import calc
+print(calc(10**9))
